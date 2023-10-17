55f4f25466a80b562eba43ac6d5f428c0710ebad
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-10-18 05:45:27 +0600

    scroll to top button added

--- src/app/(withlayout)/home/page.tsx
+++ src/app/(withlayout)/home/page.tsx
@@ -11,40 +11,119 @@ import TeamMembers from "@/components/ui/TeamMembers";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
 import UpComingServices from "@/components/ui/UpComingServices";
 import WhyChooseUs from "@/components/ui/WhyChooseUs";
+import {
+  CaretUpOutlined,
+  CaretDownOutlined 
+} from "@ant-design/icons";
 
 import { getUserInfo } from "@/services/auth.service";
 
 const { role } = getUserInfo() as any;
 console.log(role)
 
+
+
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
+const ScrollButton = () => {
+  const [showUpButton, setShowUpButton] = useState(false);
+  const [showDownButton, setShowDownButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowUpButton(true);
+      setShowDownButton(false);
+    } else {
+      setShowUpButton(false);
+      setShowDownButton(true);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  const scrollToBottom = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const buttonStyles: React.CSSProperties = {
+    position: "fixed",
+    bottom: "20px",
+    right: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    zIndex: 999,
+  };
+
+  return (
+    <div>
+      {showUpButton && (
+        <div style={buttonStyles}>
+          <Button onClick={scrollToTop}>
+            <CaretUpOutlined /> 
+          </Button>
+        </div>
+      )}
+      {showDownButton && (
+        <div style={buttonStyles}>
+          <Button onClick={scrollToBottom}>
+            <CaretDownOutlined /> 
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+const buttonStyles = {
+  position: "fixed",
+  bottom: "20px",
+  right: "20px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  zIndex: 999,
+};
+
 const MainPage = () => {
- 
   return (
     <>
+      {/* Your existing content */}
       <UMBreadCrumb
         items={[
           { label: `${role}`, link: `/${role}` },
           { label: "home", link: `/${role}/home` },
         ]}
-        style={{ marginTop: "10px",color:"black", }}
+        style={{ marginTop: "10px", color: "black" }}
       />
       <div
         style={{
           margin: "20px 0px 0px 0px",
-          
         }}
       >
         <HeroSectionPage />
         <AbailableService />
         <UpComingServices />
         <Testimonials />
-        <TeamMembers /> 
+        <TeamMembers />
         <BlogPage />
         <PartnerShip />
         <WhyChooseUs />
         <AskHere />
         <Footer />
       </div>
+
+      {/* Scroll button */}
+      <ScrollButton />
     </>
   );
 };
