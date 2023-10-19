ee2e1a14fe372980a06d9f7cd4897275579074f4
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-10-20 02:40:21 +0600

    bugs bugs bugs i avoid, bugs likes me, i can't avoid

--- src/app/(withlayout)/admin/page.tsx
+++ src/app/(withlayout)/admin/page.tsx
@@ -45,83 +45,81 @@ const Admin = () => {
         ]}
         style={{ marginTop: "10px", color: "black" }}
       />
-      <div style={centerAlign}>
-        <div style={{ overflowX: "hidden" }}>
-          <Row gutter={[16, 16]}>
-            <Col sm={24} md={12} lg={12}>
-              <img src={admin?.image} alt="service image" style={imageStyle} />
-            </Col>
-            <Col
-              sm={24}
-              md={12}
-              lg={12}
-              style={{
-                margin: "auto",
-                justifyContent: "center",
-                alignItems: "center",
-                display: "flex",
-              }}
-            >
-              <div>
-                <h1
-                  style={{
-                    color: "palevioletred",
-                    fontSize: "2rem",
-                    margin: "10px 0px",
-                  }}
-                >
-                  Name: {admin?.name}
-                </h1>
-                <h3
-                  style={{
-                    color: "SlateBlue",
-                    fontSize: "16px",
-                    marginTop: "16px",
-                  }}
-                >
-                  Email: {admin?.email}
-                </h3>
-                <h3
-                  style={{
-                    color: "SlateBlue",
-                    fontSize: "16px",
-                    marginTop: "16px",
-                  }}
-                >
-                  Number{admin?.number}
-                </h3>
+      <div style={{ overflowX: "hidden" }}>
+        <Row gutter={[16, 16]}>
+          <Col sm={24} md={12} lg={12}>
+            <img src={admin?.image} alt="service image" style={imageStyle} />
+          </Col>
+          <Col
+            sm={24}
+            md={12}
+            lg={12}
+            style={{
+              margin: "auto",
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+            }}
+          >
+            <div>
+              <h1
+                style={{
+                  color: "palevioletred",
+                  fontSize: "2rem",
+                  margin: "10px 0px",
+                }}
+              >
+                Name: {admin?.name}
+              </h1>
+              <h3
+                style={{
+                  color: "SlateBlue",
+                  fontSize: "16px",
+                  marginTop: "16px",
+                }}
+              >
+                Email: {admin?.email}
+              </h3>
+              <h3
+                style={{
+                  color: "SlateBlue",
+                  fontSize: "16px",
+                  marginTop: "16px",
+                }}
+              >
+                Number{admin?.number}
+              </h3>
 
-                <h3
-                  style={{
-                    color: "#6a5acd",
-                    fontSize: "16px",
-                    marginTop: "16px",
-                  }}
-                >
-                  Bio: {admin?.bio}
-                </h3>
-                <h3
-                  style={{
-                    color: "#6a5acd",
-                    fontSize: "16px",
-                    marginTop: "16px",
-                  }}
-                >
-                  Address: {admin?.address}
-                </h3>
-                <h1
-                  style={{
-                    color: "MediumSeaGreen",
-                    fontSize: "16px",
-                    marginTop: "16px",
-                  }}
-                >
-                  $&nbsp; BloodGroup: {admin?.bloodGroup}
-                </h1>
-              </div>
-            </Col>
-          </Row>
-        </div>
+              <h3
+                style={{
+                  color: "#6a5acd",
+                  fontSize: "16px",
+                  marginTop: "16px",
+                }}
+              >
+                Bio: {admin?.bio}
+              </h3>
+              <h3
+                style={{
+                  color: "#6a5acd",
+                  fontSize: "16px",
+                  marginTop: "16px",
+                }}
+              >
+                Address: {admin?.address}
+              </h3>
+              <h1
+                style={{
+                  color: "MediumSeaGreen",
+                  fontSize: "16px",
+                  marginTop: "16px",
+                }}
+              >
+                $&nbsp; BloodGroup: {admin?.bloodGroup}
+              </h1>
+            </div>
+          </Col>
+        </Row>
       </div>
     </>
   );
