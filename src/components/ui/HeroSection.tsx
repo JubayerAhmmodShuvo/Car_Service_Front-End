fc9b10891a72faea8b1f9892bc97fcbf356109e3
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2024-01-01 12:50:06 +0600

    color changed

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -10,24 +10,28 @@ const carouselData = [
       "https://bookmygarage.com/_next/image/?url=%2Fstatic%2Fseo-pages%2Fuploads%2Fcar-servicing-header-2.png&w=3840&q=75",
     text: "The best you want for your car. Car what people loves more and more is our responsibility to give a better service",
     buttonText: "Learn More",
+    textColor: "#F3FDE8",
   },
   {
     image:
       "https://www.gaadizo.com/gaadizo-car-service-repairs/assets/media/components/b-main-slider/dent%20repairs.jpg",
     text: "The best you want for your car. Car what people loves more and more is our responsibility to give a better service",
     buttonText: "Get Started",
+    textColor: "purple",
   },
   {
     image:
       "https://resources.servicemycar.com/upimages/blogs/Here-is-Everything-You-Need-to-Know-About-Getting-a-Car-Service-min-20210323120345.png",
     text: "The best you want for your car. Car what people loves more and more is our responsibility to give a better service",
     buttonText: "Read More",
+    textColor: "white",
   },
   {
     image:
       "https://cdn.boggsautomotive.com/wp-content/uploads/boggs-automotive-car-maintenance.jpg.webp",
     text: "We are the best and always at your service",
     buttonText: "Read More",
+    textColor: "#482121",
   },
 ];
 
@@ -42,7 +46,7 @@ const HeroSectionPage = ({}) => (
           }}
         >
           <div>
-            <h3 style={{ color: "#79155B" }}>{slide.text}</h3>
+            <h3 style={{ color: slide.textColor, fontSize:"1.5rem" }}>{slide.text}</h3>
             <Button type="primary">{slide.buttonText}</Button>
           </div>
         </div>
